14768ee40078d4bf1b21eb843b6e2cda23fcb517
pauad <132989998+pauad@users.noreply.github.com>
2023-05-09 10:08:05 +0200

    creado

--- p2/createdb.py
+++ p2/createdb.py
@@ -1,1 +1,15 @@
+import subprocess
+import sys
+import pickle
+import logging
+import time
 
+def createdb():
+  subprocess.run(['lxc', 'init', 'ubuntu2004', 'db'])
+  subprocess.run(['lxc', 'network', 'attach', 'lxdbr0', 'db', 'eth0'])
+  subprocess.run(['lxc', 'config', 'device', 'set', 'db', 'eth0', 'ipv4.address', '10.0.0.20'])
+  subprocess.run(['lxc', 'start', 'db'])
+  
+  subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'update'])
+  subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'install', '-y', 'mongodb'])
+  
