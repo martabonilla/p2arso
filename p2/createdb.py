f7237d6959385ef67f67e12ae3d1ff9ea8a3c4cf
pauad <132989998+pauad@users.noreply.github.com>
2023-05-09 10:20:27 +0200

    acbado(?)

--- p2/createdb.py
+++ p2/createdb.py
@@ -5,17 +5,30 @@ import logging
 import time
 
 def createdb():
-  subprocess.run(['lxc', 'init', 'ubuntu2004', 'db'])
-  subprocess.run(['lxc', 'network', 'attach', 'lxdbr0', 'db', 'eth0'])
-  subprocess.run(['lxc', 'config', 'device', 'set', 'db', 'eth0', 'ipv4.address', '10.0.0.20'])
-  subprocess.run(['lxc', 'start', 'db'])
-  
-  subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'update'])
-  subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'install', '-y', 'mongodb'])
-  
-  
- dbpath=/var/lib/mongodb
-logpath=/var/log/mongodb/mongodb.log
-logappend=true
-bind_ip = 127.0.0.1,10.0.0.20
-journal=true
+	subprocess.run(['lxc', 'init', 'imagenbase', 'db'])
+	subprocess.run(['lxc', 'network', 'attach', 'lxdbr0', 'db', 'eth0'])
+	subprocess.run(['lxc', 'config', 'device', 'set', 'db', 'eth0', 'ipv4.address', '10.0.0.20'])
+	subprocess.run(['lxc', 'start', 'db'])
+	
+	subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'update'])
+	subprocess.run(['lxc', 'exec', 'db', '--', 'apt', 'install', '-y', 'mongodb'])
+	
+	subprocess.run(['lxc', 'file', 'pull', 'db/etc/mongodb.conf', '.'])
+	
+	time.sleep(10)
+	with open('mongodb.conf', 'w') as fich:
+		texto = 'dbpath=/var/lib/mongodb'
+		texto2 = 'logpath=/var/log/mongodb/mongodb.log'
+		texto3 = 'logappend=true'
+		texto4 = 'bind_ip = 127.0.0.1,10.0.0.20'
+		texto5 = 'journal=true'
+	
+		fich.write(texto + '\n')
+		fich.write(texto2 + '\n')
+		fich.write(texto3 + '\n')
+		fich.write(texto4 + '\n')
+		fich.write(texto5)
+	
+	subprocess.run(['lxc', 'file', 'push', 'mongodb.conf', 'db/etc/mongodb.conf'])
+	
+	subprocess.run(['lxc', 'restart', 'db'])
