c91f7d9704ec2a03bd50c6b49355f176f756dd6a
pauad <132989998+pauad@users.noreply.github.com>
2023-05-13 19:56:01 +0200

    Update read.py

--- p2/read.py
+++ p2/read.py
@@ -1,5 +1,35 @@
 
-string = 'awaitmongoose.connect'
-with open('rest_server.js', 'r'):
-	for line in read:
-		if st
+buscado = 'await mongoose.connect'
+
+with open('app/rest_server.js', 'r') as fich:
+	data = fich.readlines()
+	
+	for i, linea in enumarate(data):
+		if buscado in linea:
+			data[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP-B + ":27017/bio_bbdd';"
+			break
+		elif:
+			continue
+			
+	
+	
+with open('app/rest_server.js', 'w') as fich:
+	fich.writelines(data)
+	
+	
+buscado2 = 'const mongoURL'
+with open('app/md-seed-config.js', 'r') as fich:
+	data2 = fich.readlines()
+	
+	for i, linea in enumarate(data2):
+		if buscado2 in linea:
+			data2[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP-B + ":27017/bio_bbdd';"
+			break
+		elif:
+			continue
+	
+	
+with open('app/md-seed-config.js', 'w') as fich:
+	fich.writelines(data2)
+
+
