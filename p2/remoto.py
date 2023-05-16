1172bdb4be7a574c750cc431d3cee7064c47b2ce
martabonilla <132989951+martabonilla@users.noreply.github.com>
2023-05-16 11:31:35 +0200

    Update remoto.py

--- p2/remoto.py
+++ p2/remoto.py
@@ -8,12 +8,12 @@ def remoto()
 	logging.basicConfig(level=logging.INFO)
 	logger = logging.getLogger(__name__)
 
-	IP-B=socket.gethostbyname(socket.gethostname())
+	IP_B=socket.gethostbyname(socket.gethostname())
 
 	print('La direcciín IP de B es ' + socket.gethostbyname(socket.gethostname()))
 		while True:
-			IP-A=input('Introduzca la IP de A: ')
-			if IP-A is '':
+			IP_A=input('Introduzca la IP de A: ')
+			if IP_A is '':
 				print('Valor invalido')
 				continue
 			
@@ -21,8 +21,8 @@ def remoto()
 				break
 	logger.info('Direcciones IP obtenidas')  
   
-	IP-B=socket.gethostbyname(socket.gethostname())
-	IPB=IP-B+':8443'
+	IP_B=socket.gethostbyname(socket.gethostname())
+	IPB=IP_B+':8443'
 	
 	
 #Permitimos el acceso remoto a las operaciones LXD
@@ -31,7 +31,39 @@ def remoto()
 #Información para la acreditación en remoto
 	subprocess.run('lxc' 'config' 'set' 'core.trust' 'password' 'ARSO')
 
-#Cambiamos el fichero rest_server
+#Cambiamos el fichero rest_server (cambiamos IP de MongoDB)
+	buscado = 'await mongoose.connect'
 
-#Cambiamos el fichero md-seed-config
+	with open('app/rest_server.js', 'r') as fich:
+		data = fich.readlines()
+	
+		for i, linea in enumarate(data):
+			if buscado in linea:
+				data[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP_B + ":27017/bio_bbdd';"
+				break
+			elif:
+				continue
+			
+	
+	
+	with open('app/rest_server.js', 'w') as fich:
+		fich.writelines(data)
+	
+
+#Cambiamos el fichero md-seed-config (cambiamos IP de MongoDB)
+	buscado2 = 'const mongoURL'
+	with open('app/md-seed-config.js', 'r') as fich:
+		data2 = fich.readlines()
+	
+		for i, linea in enumarate(data2):
+			if buscado2 in linea:
+				data2[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP_B + ":27017/bio_bbdd';"
+				break
+			elif:
+				continue
+	
+	
+	with open('app/md-seed-config.js', 'w') as fich:
+		fich.writelines(data2)
+
 
