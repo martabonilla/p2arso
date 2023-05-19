5e91fd3b1bc448b7abab35348debe87f08413a1e
martabonilla <132989951+martabonilla@users.noreply.github.com>
2023-05-19 12:12:17 +0200

    Update p2_delete1.py

--- p2/p2_delete1.py
+++ p2/p2_delete1.py
@@ -6,8 +6,15 @@ import time
 
 
 def delete1(parametro):
-	variable = 's'+ parametro
-	subprocess.run(['lxc', 'stop', variable])
-	subprocess.run(['lxc', 'delete', variable])
+	if parametro=='db':
+		subprocess.run(['lxc', 'stop', parametro])
+		subprocess.run(['lxc', 'delete', parametro])
+	elif parametro=='lb':
+		subprocess.run(['lxc', 'stop', parametro])
+		subprocess.run(['lxc', 'delete', parametro])
+	else:
+		variable = 's'+ parametro
+		subprocess.run(['lxc', 'stop', variable])
+		subprocess.run(['lxc', 'delete', variable])
 	
 
