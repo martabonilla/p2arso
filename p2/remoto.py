080e5bd9c09825a7d3e1330b8d8316d1072a3ae7
pauad <132989998+pauad@users.noreply.github.com>
2023-05-25 16:11:37 +0200

    Update remoto.py

--- p2/remoto.py
+++ p2/remoto.py
@@ -13,14 +13,7 @@ def remoto():
 	IP_B=requests.get('http://checkip.amazonaws.com').text.strip()
 
 	print('La dirección IP de B es ' + IP_B)
-	while True:
-		IP_A=input('Introduzca la IP de A: ')
-		if IP_A == '':
-			print('Valor invalido')
-			continue
-		
-		else:
-			break
+	
 	logger.info('Direcciones IP obtenidas')  
   
 	
@@ -36,42 +29,5 @@ def remoto():
 	subprocess.run(['lxc', 'config', 'set', 'core.trust_password', 'ARSO'])
 	logger.info('Password configurada') 
 
-#Cambiamos el fichero rest_server (cambiamos IP de MongoDB)
-	buscado = 'await mongoose.connect'
 
-	with open('app/rest_server.js', 'r') as fich:
-		data = fich.readlines()
-	
-		for i, linea in enumerate(data):
-			if buscado in linea:
-				data[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP_B + ":27017/bio_bbdd';"
-				break
-			else:
-				continue
-			
-	
-	
-	with open('app/rest_server.js', 'w') as fich:
-		fich.writelines(data)
-	
-	logger.info('rest_server.js configurado') 
-	
-	
-#Cambiamos el fichero md-seed-config (cambiamos IP de MongoDB)
-	buscado2 = 'const mongoURL'
-	with open('app/md-seed-config.js', 'r') as fich:
-		data2 = fich.readlines()
-	
-		for i, linea in enumerate(data2):
-			if buscado2 in linea:
-				data2[i] = "const mongoURL = process.env.MONGO_URL || 'mongodb://" + IP_B + ":27017/bio_bbdd';"
-				break
-			else:
-				continue
-	
-	
-	with open('app/md-seed-config.js', 'w') as fich:
-		fich.writelines(data2)
-	logger.info('fichero md-seed-config cambiado')
-	
 remoto()
