88d2cd8594e26fb7e72924123d5ccc5d21c48b07
Angelica123456789123 <132993342+Angelica123456789123@users.noreply.github.com>
2023-05-27 13:10:11 +0200

    Update enlarge.py

--- p2/enlarge.py
+++ p2/enlarge.py
@@ -3,7 +3,7 @@ import sys
 import pickle
 import logging
 import time
-	
+import shutil	
 def create1():
 	
 	logging.basicConfig(level=logging.INFO)
@@ -23,30 +23,58 @@ def create1():
 	subprocess.run(['rm', 'contenedores.txt'])
 	with open('contenedores.txt', 'wb') as fich:
 		stringfinal = str(variablefinal)
-		#fich.write(stringfinal)
 		pickle.dump(stringfinal, fich)
 		fich.close()
 		
+	shutil.copyfile('copiaflorero2','macetero')	
+	with open('macetero', 'a') as fich:
+	
+		texto =  'frontend firstbalance'
+		texto2 = '     bind *:80'
+		texto3 = '     option forwardfor'
+		texto4 = '     default_backend webservers'
+		texto5 = 'backend webservers'
+		texto6 = '     balance roundrobin'
+	
+		fich.write(texto + '\n')
+		fich.write(texto2 + '\n')
+		fich.write(texto3 + '\n')
+		fich.write(texto4 + '\n')
+		fich.write(texto5 + '\n')
+		fich.write(texto6 + '\n')
+		
+		for i in range(variablefinal):
+			numero = i +1
+			numeroS = 's' + str(numero)
+			fich.write('     server webserver' + str(numero) + ' ' + numeroS + ':8001'+'\n')
+		
+		fich.write('     option httpchk')
+		logger.info('Fichero haproxy modificado')
+		
+	subprocess.run(['lxc', 'file', 'push', 'macetero', 'lb/etc/haproxy/haproxy.cfg'])	
+		
+	
+	
 	variable = 's'+ str(variablefinal)
 	subprocess.run(['lxc', 'launch', 'imagenbase', variable])
 	subprocess.run(['lxc', 'stop', variable])
 	ip ='134.3.0.1' + str(variablefinal)
-	
-	subprocess.run(['lxc', 'launch', 'imagenbase', variable])
+	logger.info('Contenedor nuevo creado')
+	subprocess.run(['lxc', 'start', variable])
 	subprocess.run(['lxc', 'network', 'attach', 'lxdbr0', variable, 'eth0'])
 	subprocess.run(['lxc', 'config', 'device', 'set', variable, 'eth0', 'ipv4.address', ip])
 	subprocess.run(['lxc', 'start', variable])
-	
+	logger.info('IP del nuevo contenedor asignada')
 	direccion = variable + '/root/install.sh'
 	subprocess.run(['lxc', 'file', 'push', 'install.sh', direccion])
 	subprocess.run(['lxc', 'exec', variable,  '--', 'chmod', '+x', 'install.sh'])
-		
+	logger.info('Fichero instal.sh')	
 	direccion2 = variable + '/root'
 	subprocess.run(['lxc', 'file', 'push', '-r', 'app', direccion2])
-	subprocess.run(['lxc', 'exec', variable, '--' './install.sh'])
+	subprocess.run(['lxc', 'exec', variable, '--' ,'./install.sh'])
 	subprocess.run(['lxc', 'restart', variable])
 	subprocess.run(['lxc', 'exec', variable, '--', 'forever', 'start', 'app/rest_server.js'])
-	
+
 	
 
 	
